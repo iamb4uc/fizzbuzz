4feb077fdd525581af6904b02021a5430ff214d2
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-04-18 17:31:47 +0530

    bloat cpp

---
+++ fizzbuzz.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    for (int i = 1; i <= 100; ++i) {
+        bool is_divisible_by_3 = (i % 3 == 0);
+        bool is_divisible_by_5 = (i % 5 == 0);
+
+        (is_divisible_by_3 && is_divisible_by_5) ? std::cout << "FizzBuzz\n" :
+        (is_divisible_by_3) ? std::cout << "Fizz\n" :
+        (is_divisible_by_5) ? std::cout << "Buzz\n" :
+        std::cout << i << "\n";
+    }
+
+    return 0;
+}
+
