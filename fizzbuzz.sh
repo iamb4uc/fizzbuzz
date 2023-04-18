062c2058160d2b3269199819d2d4ecb60720c4b2
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-04-18 18:27:31 +0530

    luke smith approved POSIX COMPLIENT shell script

---
+++ fizzbuzz.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+i=1
+while [ $i -le 100 ]
+do
+  if [ $((i % 15)) -eq 0 ]; then
+    echo "FizzBuzz"
+  elif [ $((i % 3)) -eq 0 ]; then
+    echo "Fizz"
+  elif [ $((i % 5)) -eq 0 ]; then
+    echo "Buzz"
+  else
+    echo "$i"
+  fi
+  i=$((i+1))
+done
+
