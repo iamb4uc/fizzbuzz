f1882e9d6e87575463a294963e7fb6b385ee2364
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-06-24 22:27:10 +0530

    bitchless behaviour

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
   int i;
   for (i = 1; i <= 100; i++) {
-    (i % 15 == 0) ? printf("FizzBuzz\n") :
-    (i % 3 == 0) ? printf("Fizz\n") :
-    (i % 5 == 0) ? printf("Buzz\n") :
-    printf("%d\n", i);
+    (i % 15 == 0)  ? printf("FizzBuzz\n")
+    : (i % 3 == 0) ? printf("Fizz\n")
+    : (i % 5 == 0) ? printf("Buzz\n")
+                   : printf("%d\n", i);
   }
   return 0;
 }
-
